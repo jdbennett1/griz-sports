4f15bbcb444ca7f08356259c49d8043485e2faa1
jnelson544 <113126039+jnelson544@users.noreply.github.com>
2025-02-23 15:49:11 -0700

    Added containers to home page
    Added upcoming events container(possible setup to connect with database), calendar on home screen container, and created search and sort container(still need to add functionality).

--- ionic-griz-sports/src/pages/Home.tsx
+++ ionic-griz-sports/src/pages/Home.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton } from '@ionic/react';
+<<<<<<< Updated upstream
 import './Home.css';  
 import backgroundImage from '../assets/images/washgrizstad.jpg'; 
+=======
+import './Home.css';
+import EventContainer from '../components/EventContainer';
+import Calendar from '../components/Calendar';
+import SearchSortContainer from '../components/SearchSortContainer';
+>>>>>>> Stashed changes
 
 const Home: React.FC = () => {
   return (
@@ -15,12 +22,25 @@ const Home: React.FC = () => {
           </IonButtons>
         </IonToolbar>
       </IonHeader>
+<<<<<<< Updated upstream
       <IonContent>
         <div className="ion-content-background" style={{ backgroundImage: `url(${backgroundImage})` }}>
           <div className="welcome-container">
             <h2 className="welcome-title">Welcome to Griz Sports!</h2>
             <p className="subtitle">Your hub for game schedules and locations</p>
           </div>
+=======
+      
+      <IonContent className="fixed-background">
+        <h2 className="welcome-title">Welcome to Griz Sports!</h2>
+        <p className="subtitle">Your hub for game schedules and locations</p>
+        <div className="container-wrapper">
+          <div className="upcoming-events"><EventContainer /></div>
+          <div className="bottom-section">
+            <div className="calendar"><Calendar /></div>
+            <div className="search-sort"><SearchSortContainer /></div>
+        </div>
+>>>>>>> Stashed changes
         </div>
       </IonContent>
     </IonPage>
