75b4b54173bb7af4f3b1209bf1f2bae5c7640cbc
Logan Boyer <143545408+Boiler8@users.noreply.github.com>
2025-03-04 15:21:28 -0700

    API connected, login logic works, TESTUSERS

--- ionic-griz-sports/src/pages/login.tsx
+++ ionic-griz-sports/src/pages/login.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonInput, IonItem, IonList, IonText } from '@ionic/react';
+import { loginUser } from '../services/apiService';
 import './Home.css';
 import backgroundImage from '../assets/images/Landscape-Teams.png';
 
 const Login: React.FC = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleLogin = async () => {
+    if (!username || !password) {
+      setMessage("Username and password are required");
+      return;
+    }
+
+    console.log("Login button clicked");
+
+    const response = await loginUser(username, password);
+    console.log("login response:", response);
+    setMessage(response);
+
+    if (response === "Login successful!") {
+      console.log("redirecting to /Home");
+      window.location.href = "http://localhost:8100/home"; // Adjust for your domain
+    }
+  };
   return (
     <IonPage>
       <IonHeader>
@@ -19,21 +41,21 @@ const Login: React.FC = () => {
             <IonCardContent>
             <IonList>
                 <IonItem>
-                <IonInput labelPlacement="floating" value="">
+                <IonInput labelPlacement="floating" value={username} onIonInput={(e) => setUsername(e.detail.value!)}>
                 <div slot="label">
-                    Email <IonText color="danger">(Required)</IonText>
+                    Username <IonText color="danger">(Required)</IonText>
                 </div>
                 </IonInput>
                 </IonItem>
                 <IonItem>
-                <IonInput labelPlacement="floating" value="">
+                <IonInput type="password" labelPlacement="floating" value={password} onIonInput={(e) => setPassword(e.detail.value!)}>
                 <div slot="label">
                     Password <IonText color="danger">(Required)</IonText>
                 </div>
                 </IonInput>
                 </IonItem>
              </IonList>
-             <IonButton routerLink='/home'>Enter</IonButton>
+             <IonButton expand="full" onClick={handleLogin}>Enter</IonButton>
             </IonCardContent>
         </IonCard>
       </IonContent>
