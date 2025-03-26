0ba6eb76627d97a1fff2e19403a96e139b71cb35
Haiden Cramer <cramerhaiden@yahoo.com>
2025-03-25 22:36:10 -0600

    More tests for login, calendar, and helped change some of homepage tests
    New tests

--- ionic-griz-sports/src/pages/calenar.test.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import {render, screen} from '@testing-library/react'
-import Calendar from "../components/Calendar";
-import {describe, it, expect} from 'vitest'
-import '@testing-library/jest-dom';
-import { vi } from 'vitest';
--- ionic-griz-sports/src/pages/login.test.tsx
+++ ionic-griz-sports/src/pages/login.test.tsx
@@ -5,13 +5,40 @@ import {describe, it, expect} from 'vitest'
 import '@testing-library/jest-dom';
 import { vi } from 'vitest';
 
-describe("login", () => {
-
+// Haiden and JD wrote these tests
+describe("Tests for Login Page", () => {
     it("renders the defult login page", () => {
         render(<Login />)
         expect(screen.getByText("LOGIN")).toBeInTheDocument()
     });
 
-    it(" ")
+    it("Checks Log In Page Information", () => {
+        render(<Login />)
+        expect(screen.getByText("Username")).toBeInTheDocument()
+        expect(screen.getByText("Password")).toBeInTheDocument()
+        expect(screen.getByText("Sign Up")).toBeInTheDocument()
+        expect(screen.getByText("Enter")).toBeInTheDocument()
+    });
+
+    it("Makes sure login button is clickable and works", () => {
+        render(<Login />)
+        const button = screen.getByText("Enter")
+        button.click()
+        expect(button).toBeInTheDocument()
+    });
+
+    it("Makes sure sign up button is clickable and works", () => {
+        render(<Login />)
+        const button = screen.getByText("Sign Up")
+        button.click()
+        expect(button).toBeInTheDocument()
+    });
+
+    it("Makes sure username and password are required", () => {
+        render(<Login />)
+        const button = screen.getByText("Enter")
+        button.click()
+        expect(button).toBeInTheDocument()
+    });
 
 })
